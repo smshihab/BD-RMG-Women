dcdc26b2ba993e69bf5a284e926c50940837a450
SM Shihab Siddiqui <smshihab@uoregon.edu>
2022-10-11 19:02:56 -0700

    Final

--- codes/bartik_new/00_bd_garments.R
+++ codes/bartik_new/00_bd_garments.R
@@ -9,7 +9,7 @@ conflict_prefer("lag", "dplyr")
 
 # Loading the 2000-01 and 09-10 file
 bgmea_01 <- read_excel("~/large_datasets/BD Garments/00_bgmea_factory_data_final.xlsx", sheet = "bgmea_01") %>%
-  select(-c(...7, obs_year)) %>%
+  select(-obs_year) %>%
   mutate(name = tolower(name)) %>%
   filter(!duplicated(bgmea_num))
 
@@ -27,7 +27,7 @@ bgmea_13 <- read_excel("~/large_datasets/BD Garments/00_bgmea_factory_data_final
 
 # Loading the NYU
 
-bgmea_NYU <- read.csv("~/large_datasets/BD Garments/bangladesh_factory_list_NYU.csv") %>%
+bgmea_NYU <- read.csv("~/large_datasets/BD Garments/00_bangladesh_factory_list_NYU.csv") %>%
   mutate_if(is.character, ~tolower(.)) %>%
   filter(str_detect(member_of, "bgmea") | str_detect(member_of_stated, "bgmea") | str_detect(source, "bgmea"))
   
@@ -45,9 +45,9 @@ bgmea_21 <- read.csv("C:/Users/smshi/Dropbox/Research/Data/BD RMG factory data/B
 
 # Cleaning the names in 2000-01, 2009-10 and 2013 file for matching
 
-# getting rid of "['-,.:·]", then changing pvt to private, limited to ltd, & to, making fashion a separate word in all cases, and and changing a accent to a
+# getting rid of "['-,.:?]", then changing pvt to private, limited to ltd, & to, making fashion a separate word in all cases, and and changing a accent to a
 
-bgmea_01$name <- str_replace_all(bgmea_01$name, "['-,.:·\\(\\)]", "")
+bgmea_01$name <- str_replace_all(bgmea_01$name, "['-,.:?\\(\\)]", "")
   
 bgmea_01$name <- str_replace_all(bgmea_01$name, "(?<![:space:])pvt(?![:space:])|(?<![:space:])pvt|(?![:space:])pvt", " pvt ")
   
@@ -56,18 +56,19 @@ bgmea_01$name <- str_replace_all(bgmea_01$name, "(?<![:space:])limited(?![:space
 ## Changing fashions to fashion
 
 bgmea_01$name <- str_replace_all(bgmea_01$name, "fashions", " fashion ")
+
 bgmea_01$name <- str_replace_all(bgmea_01$name, "(?<![:space:])fashion(?![:space:])|(?<![:space:])fashion|(?![:space:])fashion", " fashions ")
 
 bgmea_01$name <- str_replace_all(bgmea_01$name, "&", " and ")
 
-bgmea_01$name <- str_replace_all(bgmea_01$name, "á", "a")
+#bgmea_01$name <- str_replace_all(bgmea_01$name, "?", "a")
 
 bgmea_01$name <- str_replace_all(bgmea_01$name, "\\s+", " ")
 
 
 ### 2009-10 files
 
-bgmea_09$name <- str_replace_all(bgmea_09$name, "['-,.:·\\(\\)]", "")
+bgmea_09$name <- str_replace_all(bgmea_09$name, "['-,.:?\\(\\)]", "")
 
 bgmea_09$name <- str_replace_all(bgmea_09$name, "(?<![:space:])pvt(?![:space:])|(?<![:space:])pvt|(?![:space:])pvt", " pvt ")
 
@@ -80,14 +81,14 @@ bgmea_09$name <- str_replace_all(bgmea_09$name, "(?<![:space:])fashion(?![:space
 
 bgmea_09$name <- str_replace_all(bgmea_09$name, "&", " and ")
 
-bgmea_09$name <- str_replace_all(bgmea_09$name, "á", "a")
+#bgmea_09$name <- str_replace_all(bgmea_09$name, "?", "a")
 
 bgmea_09$name <- str_replace_all(bgmea_09$name, "\\s+", " ")
 
 
 ### Cleaning 2013 files
   
-bgmea_13$name <- str_replace_all(bgmea_13$name, "['-,.:·\\(\\)]", "")
+bgmea_13$name <- str_replace_all(bgmea_13$name, "['-,.:?\\(\\)]", "")
 
 bgmea_13$name <- str_replace_all(bgmea_13$name, "(?<![:space:])pvt(?![:space:])|(?<![:space:])pvt|(?![:space:])pvt", " pvt ")
 
@@ -98,9 +99,11 @@ bgmea_13$name <- str_replace_all(bgmea_13$name, "(?<![:space:])fashion(?![:space
 
 bgmea_13$name <- str_replace_all(bgmea_13$name, "&", " and ")
 
-bgmea_13$name <- str_replace_all(bgmea_13$name, "á", "a")
+#bgmea_13$name <- str_replace_all(bgmea_13$name, "?", "a")
 
 bgmea_13$name <- str_replace_all(bgmea_13$name, "\\s+", " ")
+
+# obtaining BGMEA 2013 data for matching\
 
 bgmea_13 %>%
   filter(!duplicated(name)) %>%
@@ -108,43 +111,43 @@ bgmea_13 %>%
 
 rm("bgmea_13")
 
+# matching BGMEA 2000-01 dataset with BGMEA 2013 by name
 
 match_01 <- inner_join(bgmea_01, bgmea_13_for_matching, by = "name")
 
+# Extracting the non-matches in 2000-01 dataset
+
 not_match_01 <- anti_join(bgmea_01, match_01, by = "bgmea_num")
 
-# Using the 2021 bgmea dataset to match using the bgmea number
+# Using the 2021 BGMEA dataset to match using the BGMEA number
 
 inner_join(not_match_01, bgmea_21 %>% select(-name), by = "bgmea_num") %>%
   mutate(upazila = NA) %>%
   rbind(match_01) -> match_01
 
-
+# Extracting the non-matches in 2000-01 dataset
 not_match_01 <- anti_join(bgmea_01, match_01, by = "bgmea_num")
 
+# Extracting the non-matches in BGMEA 2013 dataset
 updated_bgmea_13_for_matching <- anti_join(bgmea_13_for_matching, match_01, by = "name")
 
+# Separating BGMEA 2000-01 non-matches into factories that are in BGMEA in 2009-10
 not_match_01_try_bgmea <- inner_join(not_match_01, bgmea_09 %>% select(bgmea_num), by = "bgmea_num")
 
+# Separating BGMEA 2000-01 non-matches into factories that are NOT in BGMEA in 2009-10
 not_match_01_try_others <- anti_join(not_match_01, not_match_01_try_bgmea, by = "bgmea_num")
-
-# rm("bgmea_09", "not_match_01")
-
 
 
 ## The following code was used to use soundex to find matches
-
 
 #stringdist_join(not_match_01_try_bgmea, updated_bgmea_13_for_matching,
                 #by = "name",
                 #method = "soundex") -> soundex_tryBGMEA13
 
+## soundex_tryBGMEA13 was exported and manually sorted for genuine matches
 #write.csv(stringdist_join, "~/large_datasets/BD Garments/01_bgmea_factory_soundexMATCH13.csv")
 
-## soundex_tryBGMEA13 was exported and manually sorted for genuine matches
-
-
-# Start running codes from here
+### Start running codes from here
 
 soundex_tryBGMEA13 <- read.csv("~/large_datasets/BD Garments/01_bgmea_factory_soundexMATCH13_completed.csv") %>%
   filter(X == 1) %>%
@@ -153,18 +156,16 @@ soundex_tryBGMEA13 <- read.csv("~/large_datasets/BD Garments/01_bgmea_factory_so
 
 match_01 <- rbind(match_01, soundex_tryBGMEA13)
 
+# separating out the soundex matches from non-matches
+
 not_match_01_try_bgmea <- anti_join(not_match_01_try_bgmea, soundex_tryBGMEA13, by = "bgmea_num")
 
-#rm("soundex_tryBGMEA13", "updated_bgmea_13_for_matching", "bgmea_13_for_matching")
-
-### Now try 21 with BGMEA number and does not work. 
-## Since both datasets has the same unique BGMEA number, checking with soundex is not needed.  
-
-### Now try BGMEA members in NYU file - but too many unmatches
-
+# All of non-mactches in 2000-01 file
 not_match_01 <- rbind(not_match_01_try_bgmea, not_match_01_try_others)
 
-##  Export the unmatched stuff to manually work on them.
+### Tried soundex matches with BGMEA members in NYU file - but too many unmatches.
+
+##  Export the unmatched BGMEA 2000-01 to manually work on them.
 
 ## write.csv(not_match_01, "~/large_datasets/BD Garments/02_not_match_01.csv", row.names = FALSE)
 
@@ -177,31 +178,62 @@ manually_matched_BGMEA_01 %>%
   mutate(Latitude = NA, Longitude = NA, fac_ad = NA, date_est = NA) %>%
   select(-remarks) -> manually_matched_BGMEA_01
 
-# Export matched data for use in awesome table
+# binding data
 
 all_matched_data_01 <- rbind(manually_matched_BGMEA_01, match_01)
 
-# Assigning machines to be their mean values
+# Assigning missing machines to be their mean values
 
 all_matched_data_01$machine[is.na(all_matched_data_01$machine)] <- mean(all_matched_data_01$machine, na.rm=T)
 
-# Only working with all_matched data
+# Separating the all_matched data to those with and without date of establishment
+
+all_matched_data_01 %>% filter(!is.na(date_est) & 
+                                 date_est < 2002) -> has_date
+
+all_matched_data_01 %>% filter(is.na(date_est)) -> has_no_date
+
+### OLS for prediction based on those that has date
+
+reg_date <- lm(date_est ~ bgmea_num, has_date)
+
+
+has_date$predicted_year <- round(reg_date$fitted.values, digits = 0)
+
+# obtaining existence estimation error rate.
+
+has_date %>%
+  mutate(exist91 = case_when(date_est <1992 ~ 1,
+                             TRUE ~ 0),
+         exist01 = case_when(date_est <2002 ~ 1,
+                             TRUE ~ 0),
+         exist91_pred = case_when(predicted_year <1992 ~ 1,
+                                  TRUE ~ 0),
+         exist01_pred = case_when(predicted_year <2002 ~ 1,
+                                  TRUE ~ 0)) -> has_date
+#(sum(abs(has_date$exist91 - has_date$exist91_pred)) + sum(abs(has_date$exist00 - has_date$exist00_pred))) / (2*2161)
+
+# providing the predicted values to those that do not have date of establishment
+
+has_no_date$date_est <- round(predict(reg_date, has_no_date), digits = 0)
+
+# existence value creator
+
+has_no_date %>%
+  mutate(exist91 = case_when(date_est <1992 ~ 1,
+                             TRUE ~ 0),
+         exist01 = case_when(date_est <2002 ~ 1,
+                             TRUE ~ 0)) -> has_no_date
+
+has_date %>% select(-c(predicted_year, exist91_pred, exist01_pred)) %>%
+  rbind(has_no_date) -> all_matched_data_01
+
+
+# cleaning environment
 
 keep = c("all_matched_data_01", "bgmea_01", "bgmea_09", "bgmea_21", "updated_bgmea_13_for_matching")
 
-
-# remove unncessary data
-
 rm(list = setdiff(ls(), keep))
-
-
-<<<<<<< HEAD
-=======
-write.csv(all_matched_data_01, "~/large_datasets/BD Garments/03_all_matched_data_01.csv", row.names = F) 
-
-
->>>>>>> fd6d7f289af1d170e60372d3e9d2940a7d32928b
-#### Working with factory data
 
 
 ### Recall that I can use information about upazila,date and fac types
@@ -212,7 +244,7 @@ write.csv(all_matched_data_01, "~/large_datasets/BD Garments/03_all_matched_data
 
 matches_09 <- inner_join(bgmea_09 %>% rename(machine09 = machine), 
                          all_matched_data_01 %>% 
-                            select(-c(name, capacity, product_type)), 
+                            select(-c(name, capacity, product_type, exist91, exist01)), 
                             by = "bgmea_num")
 
 # Assigning 2009 machines to be their mean values
@@ -228,11 +260,9 @@ matches_09 %>%
   select(-machine09) -> matches_09
 
 # finding firms that started between 2000-01 to 2009-10
-
 non_matches_09 <- anti_join(bgmea_09, bgmea_01, by = "bgmea_num")
 
-# matching them with 2001 bGMEA DATA
-
+# matching them with 2021 BGMEA data
 
 match_09_21 <- inner_join(non_matches_09, bgmea_21 %>% select(-name), by = "bgmea_num")
 
@@ -241,14 +271,43 @@ match_09_21 %>%
          exist05 = case_when(date_est <2006 ~ 1,
                       TRUE ~ 0)) -> match_09_21
 
-all_matched_data_09 <- rbind(match_09_21, matches_09)
+# Combining the BGMEA 2009-10 matches so far
+all_matched_data_09 <- rbind(matches_09, match_09_21)
 
-<<<<<<< HEAD
+# Separating the BGMEA 2009-10 matches so far
+non_matches_09 <- anti_join(non_matches_09,all_matched_data_09, by = "bgmea_num")
+
+
+# Estimating the date of establishment and will work only with those that were likely to exist in 2005
+
+reg_date <- lm(date_est ~ bgmea_num, all_matched_data_09)
+
+non_matches_09$date_est <- round(predict(reg_date, non_matches_09), digits = 0)
+
+# only keeping factories that provides 2006 estimates
+
+non_matches_09 %>%
+  filter(date_est < 2006) -> non_matches_09
+
+## Export these data for manual matching
+# write.csv(non_matches_09, "~/large_datasets/BD Garments/02_not_match_09.csv", row.names = FALSE)
+
+manually_matched_BGMEA_09 <- read.csv("~/large_datasets/BD Garments/02_manually_matched_BGMEA_09.csv")
+
+manually_matched_BGMEA_09 %>%
+  filter(upazila != "") %>%
+  mutate(exist05 = 1, Latitude = NA, Longitude = NA, fac_ad = NA) -> manually_matched_BGMEA_09
+
+# Bind the relevant 2009-10 data
+
+all_matched_data_09 <- rbind(all_matched_data_09, manually_matched_BGMEA_09)
+
+# mean machine for missing machine
+all_matched_data_09$machine[is.na(all_matched_data_09$machine)] <- mean(all_matched_data_09$machine, na.rm=T)
+
 rm(list = setdiff(ls(), c("all_matched_data_01", "all_matched_data_09")))
 
-
-### Placing the 2000 and 2001 established data in 01 file
-
+### Placing the data of factories established in 2000 and 2001 in dataset for 2001
 
 all_matched_data_01 %>%
   filter(date_est %in% c(2000, 2001)) -> matching01
@@ -259,12 +318,20 @@ all_matched_data_09 %>%
 anti_join(matching09, matching01, by = "bgmea_num") -> matching09
 
 
-all_matched_data_01 <- rbind(all_matched_data_01,
-                             matching09 %>% select(-exist05))
-  
+all_matched_data_01 <- rbind(matching09 %>% select(-exist05) %>%
+                               mutate(exist91 = 0, exist01 = 1),
+                             all_matched_data_01)
+
+
+########## manually correct the exported files fac_types ##########
+########## manually correct the exported files fac_types ##########
+########## manually correct the exported files fac_types ##########
+
+# Exporting files
 
 write.csv(all_matched_data_01, "~/large_datasets/BD Garments/03_all_matched_data_01.csv", row.names = F) 
-=======
->>>>>>> fd6d7f289af1d170e60372d3e9d2940a7d32928b
-
 write.csv(all_matched_data_09, "~/large_datasets/BD Garments/03_all_matched_data_09.csv", row.names = F)
+
+########## manually correct the exported files fac_types ##########
+########## manually correct the exported files fac_types ##########
+########## manually correct the exported files fac_types ##########
