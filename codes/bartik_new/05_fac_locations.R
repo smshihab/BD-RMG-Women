3c34334db4f7c246498bb4afd718d70a203242f8
SM Shihab Siddiqui <smshihab@uoregon.edu>
2022-10-26 19:50:35 -0700

    almost done

--- codes/bartik_new/05_fac_locations.R
+++ codes/bartik_new/05_fac_locations.R
@@ -1,14 +1,14 @@
 library(pacman)
 
-p_load(tidyverse, conflicted, janitor, sf)
+p_load(tidyverse, conflicted, janitor, sf, units)
 
-#conflict_prefer_all("dplyr")
+conflict_prefer_all("dplyr")
 #conflict_prefer("filter", "dplyr")
 #conflict_prefer("lag", "dplyr")
 #conflict_prefer("mutate", "dplyr")
 
 
-### Creating a let of factory upazila matching over time.
+### Creating upazila matchers by areas of intersection
 
 upazilas11  <- "C:/Users/smshi/OneDrive/Documents/large_datasets/BD HH or Micro data/IPUMS-I/geo3_bd2011/geo3_bd2011.shp"
 
@@ -36,7 +36,7 @@ upazilas01 <- st_as_sf(upazilas01) %>% st_transform(3106) %>%
 upazilas <- st_intersection(upazilas11, upazilas01) %>%
   mutate(overlap_size = drop_units(st_area(geometry))) %>%
   filter(overlap_size > 100) %>%
-  mutate(overlap_ratio = overlap_size/area11) %>% filter(overlap_ratio > 0.25)
+  mutate(overlap_ratio = overlap_size/area11) %>% filter(overlap_ratio > 0.2)
 
 upazilas <- upazilas %>% as.data.frame() %>%
   select(-c(parent.1, overlap_size, geometry)) %>%
@@ -63,45 +63,28 @@ upazilas91 <- st_as_sf(upazilas91) %>% st_transform(3106) %>%
 upazilas <- st_intersection(upazilas, upazilas91) %>%
   mutate(overlap_size = drop_units(st_area(geometry))) %>%
   filter(overlap_size > 100) %>%
-  mutate(overlap_ratio = overlap_size/area01) %>% filter(overlap_ratio >0.25) %>%
+  mutate(overlap_ratio = overlap_size/area01) %>% filter(overlap_ratio >0.2) %>%
   as.data.frame() %>%
   select(ipum1991, ipum2001, ipum2011)
 
+upazilas <- left_join(upazilas, upazilas91 %>% 
+                        as.data.frame() %>% select(ipum1991))
+
+
 rm(list = setdiff(ls(), "upazilas"))
 
-#load("C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/upazilas_all.RData")
+save(upazilas, file = "C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/upazilas_matcher_all.RData")
 
 # matching
 
 upazilas %>% mutate(across(everything(), ~as.integer(.))) -> upazilas
 
-load("C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/fac_upazilas.Rdata")
 
-# change all to integers
-
-factory_upazilas %>%
-  mutate(across(-upazila, ~as.integer(.x))) -> factory_upazilas
-
-factory_upazilas <- factory_upazilas %>%
-  mutate(across(-upazila, ~as.integer(.)))
-
-ninetyone_sample <- factory_upazilas$ipum1991
-twentyone_sample <- factory_upazilas$ipum2001
-twenty11_sample <-  factory_upazilas$ipum2011
+# Loading ipums data
 
 load("C:/Users/smshi/OneDrive/Documents/large_datasets/BD HH or Micro data/IPUMS-I/data_cleaned_indiv.Rdata")
 
-data %>%
-  mutate(has_fac = case_when(
-    geo3_bd1991 %in% ninetyone_sample |
-    geo3_bd2001 %in% twentyone_sample |
-    geo3_bd2011 %in% twenty11_sample ~ 1, TRUE ~0
-  )) -> data
-
-data %>%
-  filter(year == 1991 & has_fac == 1) %>%
-  select(geo3_bd1991) %>% unique()
-  
+# creating a common 1991 variable to group by
 data %>%
   filter(year == 1991) -> data1991
 
@@ -121,6 +104,7 @@ data2001 %>%
   left_join(upazilas %>% select(ipum2001, ipum1991), by = "ipum2001") %>%
   select(-ipum2001) %>% rbind(data1991) -> data
 
+
 rm("data1991", "data2001")
 
 data2011 %>%
@@ -130,14 +114,107 @@ data2011 %>%
 
 rm("data2011")
 
-lm(factory_location ~ electrification + urban + density1564, data91) %>% summary()
-%>%
-  -> data91
+# obtaining upazila estimates
 
-data2001 %>%
+data %>%
+  filter(!is.na(ipum1991)) %>%
   group_by(year, ipum1991) %>%
-  summarise(urban_share = mean(urban, na.rm=T),
+  summarise(sample_pop = n(),
+            m_ind = mean(m_ind, na.rm = T),
+            m_schooling = mean(m_schooling, na.rm=T),
+            urban_share = mean(urban, na.rm=T),
             electrification = mean(electric, na.rm=T),
             age_1564_share = sum(age >= 15 & age <=64)/n(),
             yrschool = mean(yrschool, na.rm=T)) %>% 
-ungroup() -> data_fac_loc
+  ungroup() -> data_fac_loc
+
+# Obtaining areas
+
+# 1991 dataset
+
+upazilas91  <- "C:/Users/smshi/OneDrive/Documents/large_datasets/BD HH or Micro data/IPUMS-I/geo3_bd1991/geo3_bd1991.shp"
+
+upazilas91 <- st_read(upazilas91, quiet = TRUE)
+
+# Convert to Sf and transform to Mercator Gulshan
+upazilas91 <- st_as_sf(upazilas91) %>% st_transform(3106) %>%
+  select(contains("IP")) %>%
+  clean_names() %>%
+  mutate_if(is.character,as.integer) %>%
+  mutate(area91 = st_area(geometry) %>% drop_units(),
+         area91 = area91 / 1000^2) %>%
+  as.data.frame() %>% select(-geometry)
+
+data_fac_loc <- left_join(data_fac_loc, upazilas91)
+
+data_fac_loc %>%
+  mutate(pop = ifelse(year == 2011, sample_pop/0.05, sample_pop/0.1),
+         pop_1564 = age_1564_share*pop,
+         density = pop/area91,
+         density_1564 = pop_1564/area91) %>%
+  select(-sample_pop) -> data_fac_loc
+
+# Factory data and list of upazilas with facotry
+
+load("C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/fac_upazilas.Rdata")
+
+# change all to integers
+
+factory_upazilas %>%
+  mutate(across(-upazila, ~as.integer(.x))) -> factory_upazilas
+
+factory_upazilas <- factory_upazilas %>%
+  mutate(across(-upazila, ~as.integer(.)))
+
+load("C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/factories.Rdata")
+
+rm("matched_data_09", "data", "upazilas91")
+
+matched_data_01 %>%
+  filter(exist91 == 1) %>%
+  select(upaz2011) -> has_fac91
+
+factory_upazilas %>%
+  filter(upaz2011 %in% has_fac91$upaz2011) %>% 
+  select(ipum1991) -> has_fac91
+
+matched_data_01 %>%
+  filter(exist01 == 1) %>%
+  select(upaz2011) -> has_fac01
+
+factory_upazilas %>%
+  filter(upaz2011 %in% has_fac01$upaz2011) %>% 
+  select(ipum1991) -> has_fac01
+
+has_fac91 <- has_fac91$ipum1991
+has_fac01 <- has_fac01$ipum1991
+
+data_fac_loc %>%
+  mutate(has_fac91 = case_when(ipum1991 %in% has_fac91 ~ 1, TRUE ~ 0),
+         has_fac01 = case_when(ipum1991 %in% has_fac01 ~ 1, TRUE ~ 0)) -> data_fac_loc
+
+rm("has_fac91", "has_fac01")
+
+data_fac_loc %>%
+  mutate(has_fac = pmax(has_fac91, has_fac01)) -> data_fac_loc
+
+data_fac_loc %>%
+  filter(year < 2011 & !is.na(ipum1991)) %>%
+  select(year, has_fac, has_fac91, has_fac01, electrification, urban_share, density, density_1564, yrschool) %>%
+  mutate(across(!c(year, has_fac, has_fac91, has_fac01), ~(. - mean(.))/sd(.))) -> 
+  data_fac_loc_standardized
+
+data_fac_loc %>%
+  group_by(ipum1991) %>%
+  mutate(across(c(m_ind, m_schooling, pop, pop_1564, electrification, urban_share), 
+                ~(. - dplyr::lag(.)), .names = "diff_{.col}")) %>% ungroup() -> data_share_reg
+
+load("C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/shift_shares.RData")
+  
+left_join(data_share_reg, autor_shares, by =c("ipum1991", "year")) -> data_share_reg
+
+data_share_reg %>%
+  filter(has_fac == 1) -> data_share_reg
+
+save(list = c("data_share_reg", "data_fac_loc_standardized"),
+     file = "C:/Users/smshi/Dropbox/Research/BD-RMG-Women/data/fac_location_result1.RData")
